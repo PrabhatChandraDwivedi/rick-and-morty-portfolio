330bb9747449a290da182188d8629d001224844e
PrabhatChandraDwivedi <mr.prabhatdwivedi@gmail.com>
2024-12-14 13:21:34 +0530

    Pushing changes for resposiveness and fixing css

--- src/components/BackendShowCase.js
+++ src/components/BackendShowCase.js
@@ -3,18 +3,18 @@ import { Meteors } from "../components/aceLib/MeteorEffect";
 
 export function BackendShowCase({header,body}) {
   return (
-    (<div className="">
-      <div className=" w-full relative max-w-xs">
+    (<div className="mb-10">
+      <div className=" w-full relative lg:max-w-xs">
         <div
           className="absolute inset-0 h-full w-full bg-gradient-to-r from-blue-500 to-teal-500 transform scale-[0.80] bg-red-500 rounded-full blur-3xl" />
         <div
-          className="relative shadow-xl bg-gray-900 border border-gray-800  px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-start">
+          className="min-h-60 relative shadow-xl bg-gray-900 border border-gray-800  px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col lg:justify-end justify-center items-start">
 
           <h1 className="font-bold text-xl text-white mb-4 relative z-50">
            {header}
           </h1>
 
-          <p className="font-normal text-base text-slate-500 mb-4 relative z-50">
+          <p className="font-normal text-base text-slate-300 mb-4 relative z-50">
             {body}
           </p>
 
--- src/components/WorkSection.js
+++ src/components/WorkSection.js
@@ -8,11 +8,11 @@ const WorkSection = () => (
         <h2>My Work</h2>
         <p className='mb-2'>Check out some of my UI work below:</p>
         <UIWorkShowCase/>
-        <div className='mt-36 flex justify-evenly'>
-        <BackendShowCase header={"Sonar Mapping"} body={"I worked on to create sonar Mapping for 200+ backend repos"}/>
-         <BackendShowCase header={"Job Queue Mapping"} body={"Created Jobs and Queue Mapping for backend APIs"}/>
+        <div className='mt-36 flex  flex-col lg:flex-row justify-between h-full'>
+        <BackendShowCase header={"Sonar Coverage"} body={"Worked on project to generate coverage of each line of commits made in given period of time using sonar cloud for 150+ repos"}/>
+         <BackendShowCase header={"Job & Queue Mapping"} body={"Worked on project to create a mapping for the scheduled jobs and the beans it will trigger, also mapped the beans with message queues they are attached to"}/>
          <BackendShowCase header={"UI Static Mapping"} body={"Developed logic to create static mapping for UI repos using Abstract Syntax Tree and create a master mapping using static mapping"}/>
-         <BackendShowCase header={"Backend Servers"} body={"Developed a springboot server for the react frontend. Applied SSO, jwt login as well."}/>
+         <BackendShowCase header={"Backend Servers"} body={"Created two SB servers, applied spring security, oauth2.0, handled api level role based authorization. Created 2 python servers using flask, nltk for chatbot"}/>
         </div>
     </section>
 );
